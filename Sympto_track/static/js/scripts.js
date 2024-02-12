0091f92ecdae017d731faf99916fb0426a882a23
Sarah Martinez <112424028+ladyOouf@users.noreply.github.com>
2024-02-11 22:11:52 -0600

    Update scripts.js

--- Sympto_track/static/js/scripts.js
+++ Sympto_track/static/js/scripts.js
@@ -40,3 +40,26 @@ $("form[name=login_form]").submit(function (e) {
 
     e.preventDefault();
 });
+
+$("form[name=symptom_input]").submit(function (e) {
+    e.preventDefault(); 
+
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize(); 
+
+    $.ajax({
+        url: "/user/log_pain",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function (resp) {
+            console.log("Pain data logged successfully");
+            window.location.href = "/homepage/";
+
+        },
+        error: function (resp) {
+            console.error("Error logging pain data:", resp);
+        }
+    });
+});
