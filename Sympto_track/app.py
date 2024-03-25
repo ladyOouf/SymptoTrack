1c3c8c65c5932ac81976870e537c05a783808d4f
Sarah Martinez <112424028+ladyOouf@users.noreply.github.com>
2024-03-24 20:08:04 -0500

    Update app.py

--- Sympto_track/app.py
+++ Sympto_track/app.py
@@ -3,7 +3,7 @@
 # • Description: Establishes signal to MongoDb Cluster and Sample Collections and between the frontend html pages.
 # • Programmer’s name: Sarah Martinez
 # • Data of Creation: 01.25.2023
-# • Latest Revision: 03.10.2024
+# • Latest Revision: 03.24.2024
 # • Brief description of each revision & author
 # • Preconditions: Requires my password and username created on MongoDb in order to access the cluster
 #   Username/Password are hidden and not shown. .ENV contains MongoDB URI. IP must be added to Database Cluster
@@ -16,23 +16,26 @@
 
 from flask import Flask, render_template, session, flash, redirect, url_for
 from functools import wraps
+from database import db
+from user.models import PainLog
+
 from pymongo import MongoClient
 
 from dotenv import load_dotenv
 import os
 
 # Database
-load_dotenv()
-MONGODB_URI = os.environ['MONGODB_URI']
+# load_dotenv()
+# MONGODB_URI = os.environ['MONGODB_URI']
 
 app = Flask(__name__)
 app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 
-# Connect to your MongoDB cluster:
-client = MongoClient(MONGODB_URI)
-db = client['symptotrack']
-users = db['user_creds']
-symptoms = db['pain']
+# # Connect to your MongoDB cluster:
+# client = MongoClient(MONGODB_URI)
+# db = client['symptotrack']
+# users = db['user_creds']
+# symptoms = db['pain']
 
 
 def login_required(f):
@@ -58,23 +61,36 @@ def home():
 
 @app.route('/signup/')
 def register():
-    return render_template('other html/Sign up.html')
+    return render_template('Sign up.html')
 
 
 @app.route('/homepage/')
 @login_required
 def homepage():
-    return render_template('Homepage_new.html')
+    # Fetch pain log data
+    pain_logs = PainLog().get_pain_logs(session['user']['_id'])
+
+    # Prepare data for chart
+    labels = []
+    values = []
+    descriptions = []  # Collect pain descriptions
+    for log in pain_logs:
+        labels.append(log['pain_date'])
+        values.append(log['pain_input'])
+        descriptions.append(log['pain_description'])  # Collect pain descriptions
+
+    return render_template('homepage_new.html', labels=labels, values=values, descriptions=descriptions)
 
 
 @app.route('/symptom_input/')
 @login_required
 def symptom_input():
-    return render_template('other html/symptom_input.html')
+    return render_template('symptom_input.html')
 
 
 @app.route('/profile/')
 @login_required
 def profile():
     return render_template('profile.html')
+
 
